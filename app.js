ae260cea67f651c72aa771743f9febb41932c471
Gaurav Goyal <gauravgoyal@192.168.1.5>
2024-07-10 19:35:50 +0530

    Fixed bug

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
 require('dotenv').config();
 const express = require('express');
 const csvRoutes = require('./src/routes/csvRoutes');
-const kafkaConsumer = require('./src/kafka/consumer');
+// const {runConsumer} = require('./src/kafka/consumer');
 const connectDB = require('./src/config/connectDB');
 const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT;
 
-
+connectDB();
 app.use(cors());
 app.use(express.json());
 app.use('/api', csvRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port:${port}`);
-  kafkaConsumer
-    .runConsumer()
-    .then(async () => {
-      console.log('Kafka consumer started successfully.');
-      await connectDB();
-    })
-    .catch((error) => {
-      console.error('Error starting Kafka consumer:', error);
-    });
+  // runConsumer()
+  //   .then(async () => {
+  //     console.log('Kafka consumer started successfully.');
+  //   })
+  //   .catch((error) => {
+  //     console.error('Error starting Kafka consumer:', error);
+  //   });
 });
